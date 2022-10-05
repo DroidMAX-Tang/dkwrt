f5eee91812bf1508c69896433354daefbc65658b
d4rkk3y <vuhoanhuy99@gmail.com>
2022-10-05 16:21:04 +0700

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/UTC/<+07>-7/g' package/base-files/files/bin/config_generate
 
 mv $GITHUB_WORKSPACE/package/xradio package/
